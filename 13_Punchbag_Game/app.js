9714e84948110324524e49fa5aef68e8fd4a3473
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2017-10-20 21:28:07 +0300

    Punchbag update

---
+++ 13_Punchbag_Game/app.js
@@ -0,0 +1,22 @@
+new Vue({
+  el: '#root',
+  data:{
+    healt: 100,
+    ended: false
+  },
+
+  methods:{
+    punch:function(){
+      this.healt -= 10;
+      swal(String(this.healt))
+      if (this.healt <= 0 ) {
+        this.ended = true
+
+      }
+    },
+    restart:function(){
+      this.healt = 100;
+      this.ended = false;
+    }
+  }
+});
