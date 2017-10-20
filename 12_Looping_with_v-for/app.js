7757bec122c7705f8921d6daf8917ee5f9f7f51b
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2017-10-20 20:10:11 +0300

    Looping wift v-for ninjas foreach value key and template using

--- 12_Looping_with_v-for/app.js
+++ 12_Looping_with_v-for/app.js
@@ -2,11 +2,40 @@ new Vue({
   el: '#root',
   data:{
     characters: ['Mario','Luigi','Yoshi','Bowser'],
-    ninjas: [
-      {name:'Ryu',age:25},
-      {name:'Yoshi',age:35},
-      {name:'Ken',age:55}
-    ]
+    ninjas:[
+      {
+        name:'Ryu',
+        age:25,
+        image:
+          {
+            url:'images/ryu_hayabusa.png',
+            width: '250px',
+            height: '250px'
+          }
+
+      },
+      {
+        name:'Yoshi',
+        age:35,
+        image:
+        {
+          url:'images/Yoshi.png',
+          width: '250px',
+          height: '250px'
+        }
+      },
+      {
+        name:'Mario',
+        age:45,
+        image:
+        {
+          url:'images/mario.png',
+          width: '250px',
+          height: '250px'
+        }
+      }
+      ]
+
 
   },
 
