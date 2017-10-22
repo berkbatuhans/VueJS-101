7fb24989aa47d2ac07b2d187766ef46b1d1c0224
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2017-10-22 17:59:00 +0300

    finish instances

--- 14_Multiple_Vue_Instances/app.js
+++ 14_Multiple_Vue_Instances/app.js
@@ -1,12 +1,36 @@
-new Vue({
-  el: '#root',
-  data:{
+var one = new Vue({
+  el: '#root-one',
+  data: {
+    title: 'Vue App One'
   },
 
-  methods:{
+  methods: {
 
   },
-  computed:{
-    
+  computed: {
+    greet: function() {
+      return 'Hello from app one :)';
+    }
   }
 });
+
+var two = new Vue({
+  el: '#root-two',
+  data: {
+    title: 'Vue App Two'
+  },
+
+  methods: {
+    changeTitle: function(){
+      one.title = "Title Changed";
+    }
+  },
+  computed: {
+    greet: function() {
+      return 'Yo dudes, this is app 2 speaking to ya :)';
+    }
+  }
+});
+
+
+two.title = "Changed from outside";
