4ebc9f4537c121deb9fcb4d5a2bc75756db6718f
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2017-10-30 23:11:31 +0300

    getters.js add

---
+++ Academind/vue-basics/src/store/getters.js
@@ -0,0 +1,14 @@
+export default {
+    unregisteredUser(state){
+        return state.users.filter(user => {
+            return !user.registered;
+        })
+    },
+    registrations(state){
+        return state.registrations;
+    },
+    totalRegistrations(state){
+        return state.registrations.length;
+    }
+
+};
