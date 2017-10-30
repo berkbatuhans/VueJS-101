7ab1c9d809f20f6ab31ada638085bfe7dc1dbf4e
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2017-10-30 18:16:41 +0300

    computed propery'sine mapGetters ile registrations ve total referans edildi.

--- Academind/vue-basics/src/components/Registrations.vue
+++ Academind/vue-basics/src/components/Registrations.vue
@@ -1,42 +1,39 @@
 <template>
-<div id="registrations">
-  <div class="summary">
-    <h3>Registrations</h3>
-    <h5>Total: {{total}}</h5>
-  </div>
-  <hr>
-  <div class="row" v-for="registration in registrations">
-    <h4>{{ registration.name }}</h4>
-    <span @click="unregister(registration)">Unregister</span>
-    <div class="date">
-      {{ registration.date }}
+    <div id="registrations">
+        <div class="summary">
+            <h3>Registrations</h3>
+            <h5>Total: {{total}}</h5>
+        </div>
+        <hr>
+        <div class="row" v-for="registration in registrations">
+            <h4>{{ registration.name }}</h4>
+            <span @click="unregister(registration)">Unregister</span>
+            <div class="date">
+                {{ registration.date }}
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 </template>
 
 <script>
+    import { mapGetters } from 'vuex';
 
-export default {
+    export default {
+        methods: {
+            unregister(registration) {
+                const user = this.$store.state.users.find(user => {
+                    return user.id === registration.userId;
+                });
+                user.registered = false;
+                this.$store.state.registrations.splice(this.$store.state.registrations.indexOf(registration), 1);
+            }
+        },
 
-  methods:{
-    unregister(registration){
-        const user = this.$store.state.users.find(user => {
-            return user.id === registration.userId;
-        });
-        user.registered = false;
-        this.$store.state.registrations.splice(this.$store.state.registrations.indexOf(registration),1);
+        computed: mapGetters({
+            registrations: 'registrations',
+            total: 'totalRegistrations',
+        })
     }
-  },
-  computed:{
-      registrations(){
-          return this.$store.getters.registrations;
-      },
-      total(){
-      return this.$store.getters.totalRegistrations;
-      },
-  }
-}
 </script>
 
 <style scoped>
@@ -49,23 +46,28 @@ export default {
         vertical-align: top;
         text-align: left;
     }
+
     .summary {
         text-align: center;
     }
+
     .row h4 {
         display: inline-block;
         width: 30%;
         margin: 0 0 10px 0;
         box-sizing: border-box;
     }
+
     .row span {
         width: 30%;
         color: red;
         cursor: pointer;
     }
+
     .row span:hover {
         color: darkred;
     }
+
     .date {
         display: inline-block;
         width: 38%;
